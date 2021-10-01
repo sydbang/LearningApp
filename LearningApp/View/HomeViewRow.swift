7742f73910c7171ea0038bc0093b812293680bc4
sydbang <sydney.bang94@gmail.com>
2021-10-01 12:01:13 -0400

    - create HomeViewRow card - created scrollable list of the cards in home view showing content and test

---
+++ LearningApp/View/HomeViewRow.swift
@@ -0,0 +1,73 @@
+//
+//  HomeViewRow.swift
+//  LearningApp
+//
+//  Created by Sunghee Bang on 2021-10-01.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    
+    var image:String
+    var title:String
+    var description:String
+    var count:String
+    var time:String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            HStack {
+                // Image
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                Spacer()
+                // Text
+                VStack (alignment: .leading, spacing: 10) {
+                    // Headline
+                    Text(title)
+                        .bold()
+                    // Description
+                    Text(description)
+                        .font(.caption)
+                        .padding (.bottom, 20)
+                    // Icons
+                    HStack {
+                        //Num of lessons/questions
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(count)
+                            .font(Font.system(size:10))
+                        
+                        Spacer()
+                        //Time
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(Font.system(size:10))
+                        
+                    }
+                }
+                    .padding(.leading, 20)
+                
+            }
+                .padding(.horizontal, 20)
+        }
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "Learn Swift", description: "some description", count: "10 Lessons", time: "2 Hours")
+    }
+}
